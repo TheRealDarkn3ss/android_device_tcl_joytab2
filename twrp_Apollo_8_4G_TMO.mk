431d22e5b9048aa48224709980d2cd3525719e91
TheRealDarkness <64052522+TheRealDarkn3ss@users.noreply.github.com>
2022-12-17 11:32:33 -0600

    Update twrp_Apollo_8_4G_TMO.mk

--- twrp_Apollo_8_4G_TMO.mk
+++ twrp_Apollo_8_4G_TMO.mk
@@ -8,7 +8,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common Twrp stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from Apollo_8_4G_TMO device
