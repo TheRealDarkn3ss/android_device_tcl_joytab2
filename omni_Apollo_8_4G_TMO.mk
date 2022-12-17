133bfa222080c04306da2d675b9008d91b5962fd
TheRealDarkn3ss <dgudiraz@gmail.com>
2022-12-16 23:18:21 -0600

    adding files

---
+++ omni_Apollo_8_4G_TMO.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Apollo_8_4G_TMO device
+$(call inherit-product, device/tcl/Apollo_8_4G_TMO/device.mk)
+
+PRODUCT_DEVICE := Apollo_8_4G_TMO
+PRODUCT_NAME := omni_Apollo_8_4G_TMO
+PRODUCT_BRAND := Alcatel
+PRODUCT_MODEL := 9032W
+PRODUCT_MANUFACTURER := tcl
+
+PRODUCT_GMS_CLIENTID_BASE := android-tmus-us-revc
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="9032W-user 10 QP1A.190711.020 v1C4W-0 release-keys"
+
+BUILD_FINGERPRINT := Alcatel/9032W/Apollo_8_4G_TMO:10/QP1A.190711.020/v1C4W-0:user/release-keys
